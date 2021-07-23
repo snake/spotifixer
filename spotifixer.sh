9e5f7ac511708aab0edac202bf44e227ee70572c
Jake Dallimore <jake@moodle.com>
2021-07-23 12:32:35 +0800

    Use qdbus to check playback status and conditionally stop before openuri

--- spotifixer.sh
+++ spotifixer.sh
@@ -7,5 +7,10 @@ then
 fi
 if [ $# -eq 1 ]
 then
-    qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause && qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.OpenUri "string:$1"
+    PLAYBACK_STATUS=$(qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus)
+    if [ "$PLAYBACK_STATUS" = "Playing" ]
+    then
+        qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
+    fi
+    qdbus org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.OpenUri "string:$1"
 fi
